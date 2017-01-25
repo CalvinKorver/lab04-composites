6181c7803e9ff7d7a70c1b34ebc3576ed331e965
Calvin <calvin.korver@gmail.com>
2017-01-25 11:05:10 -0800

    finished

--- composite-dom.ts
+++ composite-dom.ts
@@ -2,3 +2,41 @@ interface IDomElement {
   print();
 }
 
+/**
+ * TextNode defines a text DOM element
+ */
+class TextNode implements IDomElement {
+  private text: string;
+
+  constructor(textContent: string) {
+    this.text = textContent;
+  }  
+
+  print() {
+    console.log(this.text);
+  }
+}
+
+class DomElement implements IDomElement {
+  private text: string;
+  private element: string;
+  elements: IDomElement[];
+
+  constructor(element: string, text?:string) {
+    this.element = element;
+    this.text = text;
+    this.elements = [];
+  }
+
+  print() {
+    console.log('<${this.element}>');
+    if (this.text) {
+      console.log(this.text);
+    }
+
+    for (let element of this.elements) {
+      element.print();
+    }
+    console.log('<${this.element}>');
+  }
+}
